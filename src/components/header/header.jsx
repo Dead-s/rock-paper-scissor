f48450e23c65442efa0ff08a2339798d5af831ea
sabarisharan <sabarisharan.surendra@teknopoint.in>
2024-03-24 02:29:47 +0530

    initial commit

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,7 +4,7 @@ import styles from "./header.module.scss";
 const Header = ({ score }) => {
   return (
     <div className={styles.container}>
-      <img src="/assets/images/logo-bonus.svg" alt="Logo" />
+      <img src="./assets/images/logo-bonus.svg" alt="Logo" />
       <div className={styles.score_container}>
         <span className={styles.scoreText}>SCORE</span>
         <span className={styles.score}>{score}</span>
